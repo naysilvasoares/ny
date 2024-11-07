50dc9fef575c6043c1e22c576313b18a5bc4d1f4
naysilvasoares <00001091142336sp@al.educacao.sp.gov.br>
2024-11-07 10:27:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'livros',
+    ' É assim que acaba?',
+    ' É Assim que acaba é um livro da autora collen hoover',
+)
+
+criaCartao(
+    'filmes',
+    ' A substancia',
+    ' filme de terror lançado em 2024',
+
+criaCartao(
+    'série',
+    'the vampire diares',
+    'Uma séria passada na cidade de Mystic Falls, na Virginia',
+)
+
+criaCartao(
+    'música',
+    'Bad romance',
+    ' icone dos anos 2000, consolidou a Lady gaga como uma artista pop',
+)
